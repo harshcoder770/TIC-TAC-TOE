042fb6a92a4837165f2467c9e3727d7840ab238b
harshcoder770 <bhagatharshal32@gmail.com>
2025-03-09 17:41:18 +0530

    Last commit

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector("#reset-btn");
 
+const cross = document.querySelectorAll(".cross-btn");
+const newBtn = document.querySelectorAll(".new-game");
 
+const turnO = true;
 
-let cross = document.querySelectorAll(".cross-btn");
-let newBtn = document.querySelectorAll(".new-game");
+const appender = document.querySelector(".hide-things");
+const winMsg = document.querySelector(".win-msg");
+const winhead = document.querySelector(".win-head");
 
-let turnO = true;
+const classdo = document.querySelector(".game-end-popup");
 
-let appender = document.querySelector(".hide-things");
+// boxes.forEach((box) => {
+//   box.addEventListener("click", () => {
+//     // console.log("box was Clicked");
+//     if (turnO) {
+//       box.innerText = "O";
+//       turnO = false;
+//     } else {
+//       box.innerText = "X";
+//       turnO = true;
+//     }
+//     box.disabled = true;
 
+//     checkWinner();
+//   });
+// });
 
-
-
-
-let oWin = document.querySelector('.o-win')
-let xWin = document.querySelector('.x-win')
-let tie = document.querySelector('.tie')
-
-const resetGame = () => {
-  turnO = true;
-};
+let kiskibarri = false;
+const chnage = (abhi) => (abhi ? "X" : "O");
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    // console.log("box was Clicked");
-    if (turnO) {
-      box.innerText = "O";
-      turnO = false;
-    } else {
-      box.innerText = "X";
-      turnO = true;
-    }
+    kiskibarri = !kiskibarri;
+    box.innerText = chnage(kiskibarri);
+    // console.log(kiskibarri)
     box.disabled = true;
-
     checkWinner();
   });
 });
@@ -48,9 +51,11 @@ const winPatterns = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+const symbol = (win, loose) =>
+  `${loose} tried it's best but ${win} won the match, play one more game and let the ${loose} win.`;
 
 const checkWinner = () => {
-    let isTie  = true;
+  let isTie = true;
   for (let pattern of winPatterns) {
     let pos1Val = boxes[pattern[0]].innerText;
     let pos2Val = boxes[pattern[1]].innerText;
@@ -58,70 +63,49 @@ const checkWinner = () => {
 
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        classdo.classList.add("open");
         if (pos1Val === "O") {
-          oWin.classList.add('open')
-      
-        
-          
-        } 
-        else if (pos1Val === 'X'){
-            xWin.classList.add('open')
-            
-            
+          winhead.innerText = `${pos1Val} Won!`;
+          winMsg.innerText = symbol(pos1Val, "X");
+        } else if (pos1Val === "X") {
+          winhead.innerText = `${pos1Val} Won!`;
+          winMsg.innerText = symbol(pos1Val, "O");
         }
-
-        cross.forEach((btn) => {
-            btn.addEventListener('click' , ()=> {
-                oWin.classList.remove('open')
-                xWin.classList.remove('open')
-              
-            })
-        })
-        return
-
-
-    };
-    
-}
-}
-boxes.forEach(box => {
+      }
+    }
+  }
+  boxes.forEach((box) => {
     if (box.innerText === "") {
       isTie = false; // If any box is empty, it's not a tie
     }
   });
 
   if (isTie) {
-    tie.classList.add("open"); // Show tie popup
+    classdo.classList.add("open"); // Show tie popup
+    winMsg.innerText = `Since both player are Expert , The game ended but no one won`;
+    winhead.innerText = "Tie!";
   }
- cross.forEach((btn) => {
+  cross.forEach((btn) => {
     btn.addEventListener("click", () => {
-      tie.classList.remove("open");
+      classdo.classList.remove("open");
     });
   });
-      
-      
-    }
-    
-  
-
+};
 
 resetBtn.addEventListener("click", () => {
-  window.location.reload();
+  boxes.forEach((box) => {
+    classdo.classList.remove("open");
+    box.innerText = "";
+    box.disabled = false;
+  });
 });
-console.log(newBtn)
+// console.log(newBtn);
 newBtn.forEach((btn) => {
-    btn.addEventListener('click' , () => {
-        window.location.reload()
-    })
-})
-
-
-
-
-
-
-
-
-
-
-
+  btn.addEventListener("click", () => {
+    boxes.forEach((box) => {
+      classdo.classList.remove("open");
+      box.innerText = "";
+      box.disabled = false;
+    });
+  });
+});
